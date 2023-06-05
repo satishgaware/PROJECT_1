33144ad0feca74c2de0e5ec647314c98c7340277
satish gaware <satishgaware99@Gmail.com>
2023-06-05 23:58:41 +0530

    2st test

---
+++ src/pom/test/report/test_2_for_git.java
@@ -0,0 +1,13 @@
+package pom.test.report;
+
+public class test_2_for_git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("git test 2");
+		
+		
+	}
+
+}
